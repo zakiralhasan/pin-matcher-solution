e6bd11356619882555e6e0c665f6c3cd99e53eb3
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-08-19 23:11:02 +0600

    key match and submit section

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-
+/* ****************************  Part-1 (PIN generat section)   ************************ */
+// PIN generator function 
 function pinGenerator(){
     const generatePin = Math.round(Math.random()*10000) + '';
     return generatePin;
 };
-
+// PIN modifying function 
 function getPin(){
     const creatPin = pinGenerator();
     if(creatPin.length == 4){
@@ -13,11 +14,51 @@ function getPin(){
         return getPin();
     };
 };
-
+// PIN generate section 
 document.getElementById('pin-generator-btn').addEventListener('click', function(){
     const getPinNumber = getPin();
-    console.log(getPinNumber);
     const pinDisplayField = document.getElementById('display-pin');
-    // pinDisplayField.vlue = getPinNumber;
     pinDisplayField.value = getPinNumber;
+});
+
+/* ****************************  Part-2 (PIN matche and submit section)   ************************ */
+// key input section 
+document.getElementById('key-container').addEventListener('click', function(event){
+    const clickedKey = event.target.innerText;
+    const getKeyInputNumber = document.getElementById('display-key');
+    const previousKeyInputNumber = getKeyInputNumber.value;
+    
+    if(isNaN(clickedKey)){
+
+        if(clickedKey === 'C'){
+            getKeyInputNumber.value = '';
+
+        }else if(clickedKey === '<'){
+            const keySplit = previousKeyInputNumber.split('');
+            keySplit.pop('');
+            const keyJoin = keySplit.join('');
+            getKeyInputNumber.value = keyJoin;
+        };
+    }else{
+        let keyInputNumber = previousKeyInputNumber + clickedKey;
+        const keyInputDisplay = document.getElementById('displah-key');
+        getKeyInputNumber.value = keyInputNumber;
+    };
+});
+
+// submit section 
+document.getElementById('submit-btn').addEventListener('click', function(){
+    const pinDisplayField = document.getElementById('display-pin').value;
+    const getKeyInputNumber = document.getElementById('display-key').value;
+
+    const pinMachedSymbol = document.getElementById('pin-matched-symbol');
+    const pinDidnotMachedSymbol = document.getElementById('pin-didnot-match-symbol');
+
+if(pinDisplayField === getKeyInputNumber){
+    pinMachedSymbol.style.display = 'block';
+    pinDidnotMachedSymbol.style.display = 'none';
+}else{
+    pinDidnotMachedSymbol.style.display = 'block';
+    pinMachedSymbol.style.display = 'none';
+};
 });
